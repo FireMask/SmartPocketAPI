671b68c4e35172145ef9c55d4ee366881252b505
Joice Anely <anelylopezmo@gmail.com>
2025-09-09 22:48:19 -0600

    Installing PRIMENG

--- SmartPocketWeb2/src/app/app.config.ts
+++ SmartPocketWeb2/src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { providePrimeNG } from 'primeng/config';
+import Lara from '@primeuix/themes/lara';
 
 import { routes } from './app.routes';
 
@@ -8,6 +9,11 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
+    providePrimeNG({
+        theme: {
+            preset: Lara
+        }
+    }),
     provideRouter(routes)
   ]
 };
